93268bc0692951f31383c1901ca158be3bb9105b
JasonLu <s099113302@gmail.com>
2024-08-15 00:06:44 +0800

    first commit

---
+++ example/day4/components/home/banner/SubTitle.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <p class="banner-subtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quidem nisi sit animi alias fugiat! Obcaecati aut eligendi magni hic?</p>
+</template>
+
+<style scoped></style>
